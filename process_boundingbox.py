3bf60fe32ce2ebd292b4b029f6e32a62ac5c9179
Raghul Raj <76444923+raghulrajn@users.noreply.github.com>
2023-02-07 00:00:46 +0530

    Update process_boundingbox.py

--- process_boundingbox.py
+++ process_boundingbox.py
@@ -4,9 +4,9 @@ import copy
 
 
 def frameNorm(frame, bbox):
-            normVals = np.full(len(bbox), frame.shape[0])
-            normVals[::2] = frame.shape[1]
-            return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
+    normVals = np.full(len(bbox), frame.shape[0])
+    normVals[::2] = frame.shape[1]
+    return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
 
 
 def bbArea(box):
